f2957a6ecf3d2a399371b6198b6dc7a86f4e3332
Leo Sauberman <leosauberman@gmail.com>
2018-07-30 10:07:04 -0300

    Basic server running

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express'),
+    bodyParser = require('body-parser');
+var app = express();
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    bodyParser.json();
+    next();
+});
+
+let historico = {};
+
+app.get('/', function (req, res) {
+  res.send(historico);
+  console.info("GET / from: " + req.originalUrl);
+});
+
+app.listen(3000, function () {
+  console.info('Server listening on port 3000!');
+});
+
